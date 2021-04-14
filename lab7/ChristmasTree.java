a210d20922aa980ef4e17365abe5fb7f3d242c85
Bohdan Shcherb <73034554+bohdanshcherba@users.noreply.github.com>
2021-04-14 11:47:37 +0300

    add lab7 with changes

---
+++ lab7/ChristmasTree.java
@@ -0,0 +1,116 @@
+package javalab.lab7;
+
+public class ChristmasTree {
+    public int height = 0;
+    public String producer = "";
+    public float price = 0;
+    public String material = "";
+
+    private String color = "";
+    private int dateMade = 0;
+    private String width = "";
+    private String style = "";
+    private String treeFamily = "";
+
+    private static final int staticDate = 29;
+
+    protected int discount = 0;
+    protected String lightBulbType = "";
+
+
+    public ChristmasTree(int height, String producer, float price, String material) {
+        this.height = height;
+        this.producer = producer;
+        this.price = price;
+        this.material = material;
+    }
+
+    public ChristmasTree(int height, String producer, float price, String material, String color,
+                         int dateMade, String width, String style, String treeFamily,
+                         int discount, String lightBulbType) {
+        new ChristmasTree(height,producer,price,material);
+        this.color = color;
+        this.dateMade = dateMade;
+        this.width = width;
+        this.style = style;
+        this.treeFamily = treeFamily;
+        this.discount = discount;
+        this.lightBulbType = lightBulbType;
+    }
+
+    @Override
+    public String toString() {
+        return "LAB7.LAB7.ChristmasTree { \n" +
+                "Height=" + height + ", \n" +
+                "Producer='" + producer + "', \n" +
+                "Price=" + price + ", \n" +
+                "Material='" + material + "', \n" +
+                "Color='" + color + "', \n" +
+                "Date Made=" + dateMade + ", \n" +
+                "Width='" + width + "', \n" +
+                "Style='" + style + "', \n" +
+                "Tree Family='" + treeFamily + "', \n" +
+                "Discount=" + discount + "%, \n" +
+                "Light Bulb Type='" + lightBulbType + "', \n" +
+                "} \n";
+    }
+
+
+    public static void printStaticPutUP() {
+        System.out.println("Day when should You put up your Christmas tree is " +
+                staticDate + " November");
+    }
+
+    public void printPutUP() {
+        System.out.println("Day when should You put up your Christmas tree is " +
+                staticDate + " November");
+    }
+
+    public void resetValues(int height, String producer, float price, String material,
+                            String color, int dateMade, String width, String style,
+                            String treeFamily, int discount, String lightBulbType) {
+        new ChristmasTree(height,producer,price,material, color, dateMade, width,
+                style, treeFamily, discount, lightBulbType );
+    }
+
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getDateMade() {
+        return dateMade;
+    }
+
+    public void setDateMade(int dateMade) {
+        this.dateMade = dateMade;
+    }
+
+    public String getWidth() {
+        return width;
+    }
+
+    public void setWidth(String width) {
+        this.width = width;
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public void setStyle(String style) {
+        this.style = style;
+    }
+
+    public String getTreeFamily() {
+        return treeFamily;
+    }
+
+    public void setTreeFamily(String treeFamily) {
+        this.treeFamily = treeFamily;
+    }
+}
